86ed0f3523dd14b8a26de07cd56e9026cfe0bd78
Ty Friedman <tyfriedman@pm.me>
2024-11-05 20:20:48 -0500

    beginning of the website

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://www.theunitygardens.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Unity Gardens
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
